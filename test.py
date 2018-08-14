ca0390f08d8a61c78dab564149669a3bc3319e21
Alex <xandr6@gmail.com>
2018-08-13 23:53:27 -0400

    Expanding functionality

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-def add(par1, par2)
-  sum = par1 + par2
-  return sum
-add(5, 9)
+def add(par1, par2):
+    sum = par1 + par2
+    return sum
+
+print(add(5, 9))
+
+def sub(x,y):
+    subb= x-y
+    retunr subb
+
